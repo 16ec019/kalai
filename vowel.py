fff977ec9ea9906c2a281072c916b98b02c0e333
16ec019 <42169912+16ec019@users.noreply.github.com>
2019-06-07 10:19:02 +0530

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -1,4 +1,4 @@
-ch = string(input("Enter a character: "))
+ch = str(input("Enter a character: "))
 if(ch=='A' or ch=='a' or ch=='E' or ch =='e' or ch=='I'or ch=='i' or ch=='O' or ch=='o' or ch=='U' or ch=='u'):
     print(ch, "is a Vowel")
 else:
